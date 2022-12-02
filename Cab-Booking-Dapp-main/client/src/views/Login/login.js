b074030b57d9b81e23ecb737a26ae11039b28388
Bhanupriya111 <naikbhanupriyasingh@gmail.com>
2022-12-02 23:16:48 +0530

    icon changed,ride share steps changed

--- Cab-Booking-Dapp-main/client/src/views/Login/login.js
+++ Cab-Booking-Dapp-main/client/src/views/Login/login.js
@@ -1,111 +1,123 @@
-// import { useEffect, useState } from "react";
-// import "../auth.css";
-// import { Link } from "react-router-dom";
-// import { signinUser } from "../redux/actions";
-// import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardAvatar from "components/Card/CardAvatar.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+import Form from "react-bootstrap/Form";
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
 
-// export default function Login() {
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("");
-//     const [errmsg, setErr] = useState("");
-//     const [showPass, setShowPass] = useState(true);
-//     const [details, setDetails] = useState("");
-//     let enable = false;
 
-//     const dispatch = useDispatch();
-//     const { msg, err } = useSelector((state) => state.authReducer);
 
-//     if (email != "" && password != "") {
-//         enable = true;
-//     }
 
-//     const passCheck = () => {
-//         if (email != "" && password != "") {
-//             setErr("");
-//             dispatch(signinUser({ email, password }));
-//         } else {
-//             setErr("Fields cannot be empty");
-//         }
-//     };
+// export default function ProductList() {
+
+//     const [products, setProducts] = useState([]);
 
 //     useEffect(() => {
-//         if (msg != undefined) {
-//             setDetails(msg);
-//             setTimeout(() => {
-//                 window.location.href = "/";
-//             }, [2000]);
-//         } else {
-//             setDetails(err);
-//         }
-//     }, [msg, err]);
+//         getProducts();
+//     }, [])
+
+//     const getProducts = async () =>{
+//         let result = await fetch("http://localhost:3000/alldrivers");
+//         result = await result.json();
+//         setProducts(result);
+//     }
+//     console.warn("products", products);
+
 
 //     return (
-//         <div className="registerContainer">
-//             <div className="sec">
-//                 <div className="msg">{details}</div>
-//                 <Link to="/">
-//                     <div className="back">
-//                         <ion-icon
-//                             name="arrow-back-outline"
-//                             style={{ fontSize: 30, color: "rgb(128, 128, 128)" }}
-//                         ></ion-icon>
-//                     </div>
-//                 </Link>
-//                 <div className="innersection">
-//                     <p className="head">Login</p>
-//                     <p className="sub">
-//                         Welcome back, enjoy the cheapest and safe ride.{" "}
-//                     </p>
-//                     <div className="inputContainer">
-//                         <label className="label">Email</label>
-//                         <input
-//                             className="inp"
-//                             value={email}
-//                             onChange={(e) => setEmail(e.target.value)}
-//                             placeholder="something@example.com"
-//                         />
-//                     </div>
-//                     <div className="inputContainer">
-//                         <label className="label">Password</label>
-//                         <div className="inputBox">
-//                             <input
-//                                 className="inp"
-//                                 value={password}
-//                                 type={showPass ? "password" : "type"}
-//                                 onChange={(e) => setPassword(e.target.value)}
-//                                 placeholder="xxxxxxxxx"
-//                             />
-//                             <div className="icon" onClick={() => setShowPass(!showPass)}>
-//                                 {showPass ? (
-//                                     <ion-icon
-//                                         name="eye-outline"
-//                                         style={{ fontSize: 30, color: "rgb(177, 177, 177)" }}
-//                                     ></ion-icon>
-//                                 ) : (
-//                                     <ion-icon
-//                                         name="eye-off-outline"
-//                                         style={{ fontSize: 30, color: "rgb(177, 177, 177)" }}
-//                                     ></ion-icon>
-//                                 )}
-//                             </div>
-//                         </div>
-//                         <p className="err">{errmsg}</p>
-//                     </div>
-//                     <button
-//                         onClick={() => passCheck(password)}
-//                         data-state={enable}
-//                         className="btn"
-//                     >
-//                         LOGIN
-//                     </button>
 
-//                     <Link to="/register" className="link">
-//                         <span style={{ color: "black" }}>Not having an account?</span>{" "}
-//                         Register your account
-//                     </Link>
-//                 </div>
-//             </div>
-//             <div className="sec color"></div>
-//         </div>
-//     );
+
+
+
+//     )
 // }
+
+// import React, { useState } from "react";
+
+
+
+// import Button from "react-bootstrap/Button";
+
+// import "./Login.css";
+
+export default function Login() {
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+
+    return email.length > 0 && password.length > 0;
+
+  }
+
+  function handleSubmit(event) {
+
+    event.preventDefault();
+
+  }
+
+  return (        <Card>
+              <CardHeader color="primary">
+                <h4 >LOG IN</h4>
+                
+              </CardHeader>
+              <CardBody>
+<div className="Login">
+
+      <Form onSubmit={handleSubmit}>
+      <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      inputProps={{
+                        // onChange: (e) => handleChange(e),
+                        type: "email"
+                      }}
+                      labelText="Email Address"
+                      id="email"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                      <CustomInput
+                        inputProps={{
+                          // onChange: (e) => handleChange(e),
+                          type: "password"
+                        }}
+                        labelText="Enter Password"
+                        id="password"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                      />
+                    </GridItem>
+        
+        <Button 
+                  variant="contained"
+                  color="primary"
+                  
+                  onClick=""
+                >
+                  Book me
+              </Button>
+
+      </Form>
+
+    </div>
+              </CardBody>
+            </Card>
+
+    
+
+  );
+
+}
+
+
--- Cab-Booking-Dapp-main/client/src/views/allDrivers/drivers.js
+++ Cab-Booking-Dapp-main/client/src/views/allDrivers/drivers.js
@@ -1,51 +1,3 @@
-// import React, { useEffect, useState } from "react";
-
-// export default function ProductList() {
-
-//     const [products, setProducts] = useState([]);
-
-//     useEffect(() => {
-//         getProducts();
-//     }, [])
-
-//     const getProducts = async () =>{
-//         let result = await fetch("http://localhost:3000/alldrivers");
-//         result = await result.json();
-//         setProducts(result);
-//     }
-//     console.warn("products", products);
-
-
-//     return (
-//         <div className="Drivers-list">
-//             <h3>All Drivers List</h3>
-//             <ul>
-//                 <li>S.no</li>
-//                 <li>Name</li>
-//                 <li>Price</li>
-//                 <li>contact</li>
-//                 <li>Car No</li>
-//                 <li>NO of Seats</li>
-//                 {/* <li>Company</li> */}
-//             </ul>
-
-//             {
-//                 products.map((item, index) =>
-//                     <ul>
-//                         <li>{index + 1}</li>
-//                         <li>{item.name}</li>
-//                         <li>{item.email}</li>
-//                         <li>{item.contact}</li>
-//                         <li>{item.carNo}</li>
-//                         <li>{item.noOfSeats}</li>
-//                     </ul>
-
-//                 )
-//             }
-//         </div>
-//     )
-// }
-
 import React, { useEffect, useState } from "react";
 import './drivers.css';
 import { Button } from "@material-ui/core";
@@ -73,34 +25,6 @@ export default function ProductList() {
 
 
     return (
-        // <div className="Drivers-list">
-        //     <h3>All Drivers List</h3>
-        //     <ul >
-        //         <li>S.no</li>
-        //         <li>Name</li>
-        //         <li>Price</li>
-        //         <li>contact</li>
-        //         <li>Car No</li>
-        //         <li>NO of Seats</li>
-        //         {/* <li>Company</li> */}
-        //     </ul>
-
-            // {
-            //     products.map((item, index) =>
-            //         <ul>
-            //             <li>{index + 1}</li>
-            //             <li>{item.name}</li>
-            //             <li>{item.email}</li>
-            //             <li>{item.contact}</li>
-            //             <li>{item.carNo}</li>
-            //             <li>{item.noOfSeats}</li>
-            //         </ul>
-
-            //     )
-            // }
-        // </div>
-
-
         <Card>
               <CardHeader color="primary">
                 <h4 >Driver List</h4>
@@ -121,7 +45,6 @@ export default function ProductList() {
 
           </thead>
           <tbody>
-
             {
                 products.map((item, index) =>
                     <tr className="Drivers-list-main">
@@ -152,4 +75,3 @@ export default function ProductList() {
 
     )
 }
-
